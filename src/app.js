0dfbea4f933741eabef1e65e5de84eefa373a522
Robiul1711 <ashiq.web.dev@gmail.com>
2025-12-07 17:03:20 +0600

    create contact

--- src/app.js
+++ src/app.js
@@ -3,8 +3,9 @@ import cors from "cors";
 import authRoutes from "./routes/auth.routes.js";
 import projectRoutes from "./routes/project.routes.js";
 import blogRoutes from "./routes/blog.routes.js";
-import contactRoutes from "./routes/contact.routes.js";
-
+import contactRoutes from "./routes/contact.routes.js"
+import contactInfoRoutes from "./routes/contactInfo.routes.js";
+import footerRoutes from "./routes/footer.routes.js"
 const app = express();
 
 // Middleware
@@ -16,6 +17,9 @@ app.get(("/", (req, res) => res.status(200).json({ message: 'Welcome to my Portf
 // Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/projects", projectRoutes);
+app.use("/api", contactRoutes);
+app.use("/api", contactInfoRoutes);
+app.use("/api", footerRoutes);
 // app.use("/api/blogs", blogRoutes);
 // app.use("/api/contact", contactRoutes);
 
--- src/models/Project.js
+++ src/models/Project.js
@@ -1,13 +1,18 @@
 import mongoose from "mongoose";
 
-const projectSchema = new mongoose.Schema({
-  title: String,
-  description: String,
-  stack: String,
-  technologies: [String],
-  github: String,
-  live: String,
-  image: String,
-}, { timestamps: true });
+const projectSchema = new mongoose.Schema(
+  {
+    title: String,
+    description: String,
+    stack: String,
+    technologies: [String],
+    github: String,
+    live: String,
+    image: String,
+    popular: { type: Boolean, default: false },
+  },
+  { timestamps: true }
+);
 
 export default mongoose.model("Project", projectSchema);
+
